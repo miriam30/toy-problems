fbb99bbb0557489d59d13389425a895844d3cea0
miriam30 <miriam.alvarez.p96@gmail.com>
2018-01-16 10:14:04 -0600

    toy problem one

--- problems/romanNumeralTranslator/romanNumeralTranslator.js
+++ problems/romanNumeralTranslator/romanNumeralTranslator.js
@@ -16,18 +16,31 @@
  * You should return `null` on invalid input.
  */
 
-var DIGIT_VALUES = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
-};
+ var DIGIT_VALUES = {
+   I: 1,
+   V: 5,
+   X: 10,
+   L: 50,
+   C: 100,
+   D: 500,
+   M: 1000
 
-var translateRomanNumeral = function(romanNumeral) {
-// TODO: Implement me!
-/* START SOLUTION */
-  /* END SOLUTION */
-};
+
+ };
+
+ var translateRomanNumeral = function(romanNumeral) {
+
+ translateRomanNumeral.protoype = Object.create(DIGIT_VALUES)
+
+   for(var key in DIGIT_VALUES){
+     if(DIGIT_VALUES.hasOwnProperty(key)){
+       if(DIGIT_VALUES[key] < DIGIT_VALUES[key]){
+         return DIGIT_VALUES[key] + DIGIT_VALUES[key]
+       } else if(romanNumeral[0]> romanNumeral[1]){
+         return romanNumeral[0] - romanNumeral[1]
+       }
+     }else{
+       return null
+     }
+   }
+ };
